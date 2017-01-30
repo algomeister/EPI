2165fc716b11fb81d592798ff9571f9fa7b28aa4
algomeister <mohitguptatihom@gmail.com>
2017-01-30 17:47:54 +0530

    Update sudoku.cpp

--- sudoku.cpp
+++ sudoku.cpp
@@ -24,43 +24,40 @@ int main()
 				m = (i/3)*3 + j/3;
 				check[arr[i][j] + m*10] += 1;
 				if(check[arr[i][j] + m*10] > 1)
-				{
-					cout << "Oops1111!" << " i= " << i << " j= " << j << endl;
 					flag = 1;
+			}
+		}
+	}
+	if(!flag)
+	{
+		for(int i=0;i<90;i++)
+			check[i]= 0;
+		for(int i=0;i<9;i++)
+		{
+			for(int j=0;j<9;j++)
+			{
+				if(arr[i][j])
+				{
+					check[arr[i][j] + i*10] += 1;
+					if(check[arr[i][j] + i*10] > 1)
+						flag = 1;
 				}
 			}
 		}
 	}
-	for(int i=0;i<90;i++)
-		check[i]= 0;
-	for(int i=0;i<9;i++)
+	if(!flag)
 	{
-		for(int j=0;j<9;j++)
+		for(int i=0;i<90;i++)
+			check[i]= 0;
+		for(int i=0;i<9;i++)
 		{
-			if(arr[i][j])
+			for(int j=0;j<9;j++)
 			{
-				check[arr[i][j] + i*10] += 1;
-				if(check[arr[i][j] + i*10] > 1)
+				if(arr[j][i])
 				{
-					cout << "Oops2222222!" << " i= " << i << " j= " << j << endl;
-					flag = 1;
-				}
-			}
-		}
-	}
-	for(int i=0;i<90;i++)
-		check[i]= 0;
-	for(int i=0;i<9;i++)
-	{
-		for(int j=0;j<9;j++)
-		{
-			if(arr[j][i])
-			{
-				check[arr[j][i] + i*10] += 1;
-				if(check[arr[j][i] + i*10] > 1)
-				{
-					cout << "Oops33333333!" << " i= " << i << " j= " << j << endl;
-					flag = 1;
+					check[arr[j][i] + i*10] += 1;
+					if(check[arr[j][i] + i*10] > 1)
+						flag = 1;
 				}
 			}
 		}
