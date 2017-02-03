6b7e32f34a53d658d0f6a750dcad60c728f415e4
Mohit Gupta <mohitguptatihom@gmail.com>
2017-02-03 23:25:23 +0530

    Add files via upload

---
+++ 7_6_even_odd.cpp
@@ -0,0 +1,127 @@
+#include <iostream>
+using namespace std;
+
+template <class T>
+class LinkedList{
+    struct node{
+        T x;
+        node *next;
+    };
+    public:
+    LinkedList(){
+        head = NULL;
+        curr = NULL;
+    }
+    node* getHead(){
+        return head;
+    }
+    node* getCurrent(){
+        return curr;
+    }
+    void addNode(struct node* b){
+        b->next = head;
+        head = b;
+        if(curr == NULL)
+            curr = head;
+    }
+    void addValue(T a){
+        node *n = new node;
+        n->x = a;
+        n->next = head;
+        head = n;
+        if(curr == NULL)
+            curr = head;
+    }
+    void rewind(){
+        curr = head;
+    }
+    void next(){
+        if( curr != NULL )
+            curr = curr->next;
+    }
+    int getValue(){
+        if( curr != NULL )
+            return curr->x;
+        return 0;
+    }
+    int hasValue(){
+        return ( curr != NULL ? true : false );
+    }
+    void evenOddSort(){
+        rewind();
+        node *temp1, *temp2 = head->next;
+        int i = 1;
+        while(temp2->next!=NULL){
+            int j = i, k = i-1;
+            temp1 = temp2 = curr->next;
+            while(j && curr->next->next){
+                curr->next = curr->next->next;
+                j--;
+            }
+            while(k && temp2->next)
+                {
+                temp2 = temp2->next;
+                k--;
+            }
+            if(!j){
+                if(curr->next->next)
+                    temp2->next = curr->next->next;
+                else
+                    temp2->next = NULL;
+                curr->next->next = temp1;
+                next();
+                i++;
+            }
+            else
+                {
+                curr->next = temp1;
+                temp2->next = NULL;
+            }
+        }
+        rewind();
+    }
+
+    private:
+    node *head;
+    node *curr;
+};
+
+
+
+int main(){
+    LinkedList<int> L;
+    L.addValue(12);
+    L.addValue(11);
+    L.addValue(10);
+    L.addValue(9);
+    L.addValue(8);
+    L.addValue(7);
+    L.addValue(6);
+    L.addValue(5);
+    L.addValue(4);
+    L.addValue(3);
+    L.addValue(2);
+    L.addValue(1);
+    L.addValue(0);
+    L.rewind();
+    while(L.hasValue())
+        {
+        cout << "BEFORE----Value: " << L.getValue();
+        if(L.getValue()/10 < 1)
+            cout << " ";
+        cout << "  Pointer: " << L.getCurrent() << endl;
+        L.next();
+    }
+    cout << "\n----------------------------------------------------\n" << endl;
+
+    L.evenOddSort();
+    while(L.hasValue())
+        {
+        cout << "AFTER-----Value: " << L.getValue();
+        if(L.getValue()/10 < 1)
+            cout << " ";
+        cout << "  Pointer: " << L.getCurrent() << endl;
+        L.next();
+    }
+    return 0;
+}
